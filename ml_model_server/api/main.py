2cec11eda3e890d7cad3bb9f1cc7af731b9d0391
spandan3001 <73657279+spandan3001@users.noreply.github.com>
2024-01-19 02:39:33 +0530

    Update main.py

--- ml_model_server/api/main.py
+++ ml_model_server/api/main.py
@@ -10,17 +10,17 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 MODEL = tf.keras.models.load_model("../saved_models/1")
 
@@ -51,5 +51,5 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=10000)
+    uvicorn.run(app)
 
