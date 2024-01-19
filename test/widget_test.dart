693fa1040e2a4f9f069575f70b61597c2f6189c5
spandanmn <tyian3001@gmail.com>
2024-01-19 18:14:04 +0530

    functions update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:plant_disease_detection/login_register_app.dart';
 
 import 'package:plant_disease_detection/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoginRegisterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
