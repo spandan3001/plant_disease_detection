212a29fda2d9960dc2478538271eeb78345f2a70
spandanmn <tyian3001@gmail.com>
2024-01-19 16:16:10 +0530

    final

--- ml_model_server/api/main.py
+++ ml_model_server/api/main.py
@@ -21,33 +21,34 @@ app = FastAPI()
 # )
 
 MODEL = tf.keras.models.load_model("../potatoes.h5")
-# CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+# MODEL = tf.keras.models.load_model("../saved_models/1")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
-# @app.get("/ping")
-# async def ping():
-#     return "Hello, I am alive"
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
 
-# def read_file_as_image(data) -> np.ndarray:
-#     image = np.array(Image.open(BytesIO(data)))
-#     return image
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
 
-# @app.post("/predict")
-# async def predict(
-#     file: UploadFile = File(...)
-# ):
-#     image = read_file_as_image(await file.read())
-#     img_batch = np.expand_dims(image, 0)
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
     
-#     predictions = MODEL.predict(img_batch)
+    predictions = MODEL.predict(img_batch)
 
-#     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-#     confidence = np.max(predictions[0])
-#     return {
-#         'class': predicted_class,
-#         'confidence': float(confidence)
-#     }
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
 
 if __name__ == "__main__":
-    uvicorn.run(app,host='0.0.0.0', port=8000)
+    uvicorn.run(app,host='localhost', port=8000)
     
 
